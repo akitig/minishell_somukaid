bda5ba5530188f51bf47b6a22f3c585bb6071f8f
Soshi Mukaida <somukaid@c6r4s6.42tokyo.jp>
2025-03-14 13:03:10 +0900

    #6 infinity & add_history

--- playground/readline.c
+++ playground/readline.c
@@ -1,12 +1,26 @@
 #include <readline/readline.h>
+#include <readline/history.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // char *readline (prompt)
 
 int	main(void)
 {
 	char *prompt = "minishell >";
-	char *ret = readline(prompt);
-	printf("ret:%s\n", ret);
+	while (1)
+	{
+
+	//	char *prompt = "minishell >";
+		char *ret = readline(prompt);
+		printf("ret:%s\n", ret);
+		if (ret == NULL)
+		{
+			printf("minishell is end.\n");
+			break ;
+		}
+		add_history(ret);
+		//free(ret);
+	}
 	return (0);
 }
