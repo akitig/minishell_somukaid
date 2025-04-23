9fff5e0c06bf19b02fbf34028695f0b25b589751
Soshi Mukaida <somukaid@c6r4s3.42tokyo.jp>
2025-04-23 18:22:00 +0900

    #35 builtin forward

--- srcs/main_org.c
+++ srcs/main_org.c
@@ -3,35 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: somukaid <somukaid@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: akunimot <akitig24@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 13:50:37 by akunimot          #+#    #+#             */
-/*   Updated: 2025/04/23 16:31:38 by somukaid         ###   ########.fr       */
+/*   Updated: 2025/03/28 14:12:14 by akunimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_list	*env_init(char **envp)
-{
-	size_t	i;
-	t_list	*tmp;
-	t_list	*head;
-
-	i = 0;
-	head = NULL;
-	if (envp == NULL || *envp == NULL)
-		return (NULL);
-	while (envp[i])
-	{
-		tmp = ft_lstnew(ft_strdup(envp[i]));
-		ft_lstadd_back(&head, tmp);
-		i++;
-	}
-	return (head);
-}
-
-void	minishell(char *line, t_list *envp_list)
+void	minishell(char *line)
 {
 	t_token	*token;
 	t_token	*head;
@@ -60,29 +41,25 @@ void	minishell(char *line, t_list *envp_list)
 	expansion(&node);
 	printf("----- After expansion -----\n");
 	print_node(node);
-	printf("----- op_command -----\n");
-	op_command(node, envp_list);
 	free_tokens(head);
 	free(line);
 }
 
-int	main(int ac, char **av, char **en)
+int	main(int ac, char **av)
 {
 	char	*line;
 	char	*prompt;
-	t_list	*envp_list;
 
 	prompt = "minishell > ";
 	(void)ac;
 	(void)av;
 	if (isatty(fileno(stdin)))
 	{
-		envp_list = env_init(en);
 		while ((line = readline(prompt)))
 		{
 			if (*line)
 				add_history(line);
-			minishell(line, envp_list);
+			minishell(line);
 		}
 	}
 	else
